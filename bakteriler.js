7663963895b6da2742ebd900835297a55aedf041
occ55 <onurcamci55@gmail.com>
2017-06-17 02:00:08 +0300

    hotfix

--- bakteriler.js
+++ bakteriler.js
@@ -536,6 +536,7 @@ Bakteriler.push({
   Solunum: "Fakultatif_Anaerob",
   Enzimler: ["Indol","Katalaz","Mannitol","Hemolizin",Aciklamali("Laktoz","Salmonella Ve Shigella dan ayirir"),Aciklamali("Sakkaroz","Bazen")],
   Hareket: ["Hareketli","Hareketsiz"],
+  Kapsul: Aciklamali("Kapsullu","Bazilari"),
   Antijenler: ["O","K","H","P"],
   KulturOrtami: [{
     Name: "MacConkey Agar",
