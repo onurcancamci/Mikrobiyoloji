71fdcef2a458360e13d77f8f19bbc49fa1ad53fd
occ55 <onurcamci55@gmail.com>
2017-06-28 15:19:40 +0300

    Salmonella eklendi

---
+++ Bakteriler/Salmonella.js
@@ -0,0 +1,20 @@
+Bakteriler.push({
+  AileAdi: "Enterobacteriaceae",
+  CinsAdi: "Salmonella",
+  TurAdi: "typhi",
+  Gram: "Negative",
+  Shape:  "Basil",
+  Solunum: "Fakultatif_Anaerob",
+  Enzimler: ["Katalaz","H2S"], //baska enzim olabilir
+  Hareket: "Hareketli",
+  //Antijenler: [],
+  KulturOrtami: [{
+    Name: "MacConkey Agar",
+    Gorunus: ["Soluk"],
+    Aciklama: ["Baska agarlarda var eklenicektir"],
+  }],
+  Direnc : ["Vankomisin"],
+  Duyarlilik: ["Kolistin"],
+  VirualanFaktorler: [{Name: "Flagella", Aciklama: "Peritrichous"}],
+  //Aciklama : [],
+});
