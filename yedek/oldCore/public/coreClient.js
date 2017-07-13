f7df2cb71c646fcf57b734a7dc64dfe84ac8eea1
occ55 <onurcamci55@gmail.com>
2017-07-13 17:28:29 +0300

    MOAR CHANGES

--- yedek/oldCore/public/coreClient.js
+++ yedek/oldCore/public/coreClient.js
@@ -4,9 +4,12 @@ let socket = io();
 
 socket.on("connect" , function (a,b) {
   console.log(a,b);
-})
+});
 
-
+socket.emit("login", {
+  username: "onurcan",
+  password: "asdf"
+});
 
 
 
---
+++ yedek/oldCore/server/main.js
@@ -0,0 +1,17 @@
+
+let {Loader, Bus} = require("./helpers.js");
+
+
+
+let core = Loader.LoadToNewCore("Bakteriler",["CinsAdi", "TurAdi", "SubTur"]);
+
+
+Bus.Open();
+
+
+
+
+
+
+
+
